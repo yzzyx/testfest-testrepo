d4c844bb59f324af7589514142eb31fb2082b5d5
Elias Norberg <xyzzy@kudzu.se>
2014-12-07 16:56:03 +0100

    initial commit

---
+++ runtest.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+print """
+test_get_ancestors (organisation.tests.OUTest) ... ok
+test_get_by_dn (organisation.tests.OUTest) ... ok
+test_get_children (organisation.tests.OUTest) ... ok
+test_create_duplicate (organisation.tests.PersonTest) ... ok
+test_get_by_username (organisation.tests.PersonTest) ... ok
+test_get_nonexistant (organisation.tests.PersonTest) ... ok
+test_get_user (organisation_api.tests.PersonAPITest) ... ok
+
+----------------------------------------------------------------------
+Ran 7 tests in 0.036s
+
+OK
+"""
+
+return 0
